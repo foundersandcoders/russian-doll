a419c0b580b8760693ec31561092b5818e4056b9
besartshyti <besartshyti@gmail.com>
2015-04-29 19:32:46 +0100

    add standard responses and tests

---
+++ test/map.test.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var map = require("../lib/map.js");
+var test = require("tape");
+
+test("Map function", function (t){
+
+	t.test("should return query string", function (st){
+
+		var obj = {
+			name: "Will"
+		};
+
+		var string = map(obj);
+
+		st.equals(string, "?q=name:Will", "right string");
+		st.end();
+	});
+	t.test("should return a query string given large obj", function (st){
+
+		var obj = {
+			name: "Will",
+			age: 22,
+			county: "London",
+			job: "pizza chef"
+		};
+
+		var string = map(obj);
+
+		st.equals(string, "?q=name:Will AND age:22 AND county:London AND job:pizza chef");
+		st.end();
+	});
+});
--- test/russian-doll.test.js
+++ test/russian-doll.test.js
@@ -3,245 +3,350 @@
 var test = require("tape");
 var instantiate = require("..");
 var request = require("../lib/request.js");
+var is = require("torf");
 
 var opts = {
-  index: "index",
-  type: "articles",
-  host: process.env.ES_HOST,
-  port: Number(process.env.ES_PORT)
+	index: "index",
+	type: "articles",
+	host: process.env.ES_HOST,
+	port: Number(process.env.ES_PORT)
 };
 
 var articles = instantiate(opts);
 
+test("if not config", function (t){
 
-test("wipe db", function (t) {
+	var wrongConfig = {};
 
-  var opts = {
-    method: "DELETE",
-    uri: "http://127.0.0.1:9200/index/articles/"
-  };
-  request(opts, function () {
-
-    t.end();
-  });
-
+	try {
+		instantiate(wrongConfig);
+		t.fail("test should not be executed");
+	}catch(e){
+		t.ok(e, "should throw error");
+		t.end();
+	}
 });
 
+test("NOT A TEST wipe db", function (t) {
+
+	var opts = {
+		method: "DELETE",
+		uri: "http://127.0.0.1:9200/index/articles/"
+	};
+	request(opts, function () {
+
+		t.end();
+	});
+});
 
 test("instantiate should return an object", function (t) {
 
-  t.equals(typeof articles, "object", "instantiate returns object");
-  t.end();
+	t.equals(typeof articles, "object", "instantiate returns object");
+	t.end();
 });
-
 
 test("object should have a create, findOne, update, delete and query methods", function (t) {
 
-  ["create", "findOne", "update", "del"].forEach(function(q) {
+	["create", "findOne", "update", "del"].forEach(function(q) {
 
-    t.ok(articles.hasOwnProperty(q), "object has " + q + " property");
-  });
+		t.ok(articles.hasOwnProperty(q), "object has " + q + " property");
+	});
 
-  t.end();
+	t.end();
 });
-
 
 test("#create should create new entry in database", function (t) {
 
-  articles.create({
-    name: "hello",
-    message: "yes",
-    id: 1234
-  }, function (e, r) {
+	var obj = {
+		message: "yes",
+		id: "1234"
+	};
 
-    t.notOk(e, "no error received");
-    t.ok(r.created, "object created");
+	articles.create(obj, function (e, r) {
 
-    var opts = {
-      method: "GET",
-      uri: "http://127.0.0.1:9200/index/articles/1234"
-    };
-    request(opts, function (e, r) {
 
-      t.ok(r.found, "object created");
-      t.end();
-    });
-  });
+		t.notOk(e, "no error received");
+		t.deepEqual(r, obj, "object created");
+
+		var opts = {
+			method: "GET",
+			uri: "http://127.0.0.1:9200/index/articles/1234"
+		};
+		request(opts, function (e, r) {
+
+			t.ok(r.found, "object created");
+			t.end();
+		});
+	});
 });
 
-test("#create should with id", function (t) {
+test("#create should without id", function (t) {
 
-  articles.create({
-    name: "hello",
-    message: "yes",
-  }, function (e, r) {
+	var obj = {
+		name: "hello",
+		message: "yes"
+	};
 
-    t.notOk(e, "no error received");
-    t.ok(r.created, "object created");
-    var opts = {
-      method: "GET",
-      uri: "http://127.0.0.1:9200/index/articles/" + r._id
-    };
-    request(opts, function (e, r) {
+	articles.create(obj, function (e, r) {
 
-      t.ok(r.found, "object created");
-      t.end();
-    });
-  });
+		t.notOk(e, "no error received");
+		t.ok(r.id, "should have an id");
+		t.end();
+	});
 });
-
-
 
 test("#findOne should return match from database if there is one", function (t) {
 
-  articles.findOne({
-    id: 1234
-  }, function (e, r) {
+	var obj = {id: "1234"};
 
-    t.notOk(e, "no error returned");
-    t.ok(r.found, "object found");
-    t.ok(r._source.name, "hello", "correct obj returned");
-    t.end();
-  });
+	articles.findOne(obj, function (e, r) {
+
+		console.log(r);
+
+		t.notOk(e, "no error returned");
+		t.equal(r.id, obj.id, "object found");
+		t.end();
+	});
 });
 
-test("#findOne should return err if no matches", function (t) {
+test("#findOne should return empty object if no matches", function (t) {
 
+	var obj = {id:8264};
 
-  articles.findOne({
-    id: 4321
-  }, function (e, r) {
+	articles.findOne(obj, function (e, r) {
 
-    t.ok(e, "error returned");
-    t.notOk(r, "no data passed to cb");
-    t.end();
-  });
+		t.notOk(e, "error not returned");
+		t.ok(is.type(r, "object"), "object returned");
+		t.notOk(is.ok(r), "empty object returned");
+		t.end();
+	});
 });
 
 test("#findOne should return err if no id", function (t) {
 
+	var obj = {hid: 4321};
 
-  articles.findOne({
-    hid: 4321
-  }, function (e, r) {
+	articles.findOne(obj, function (e, r) {
 
-    t.ok(e, "error returned");
-    t.notOk(r, "no data passed to cb");
-    t.end();
-  });
-});
-
-
-
-test("#update should update entry in database", function (t) {
-
-
-  articles.update({
-    id: 1234
-  }, {
-    name: "bob"
-  }, function (e) {
-
-    t.notOk(e, "error not returned");
-
-    var opts = {
-      method: "GET",
-      uri: "http://127.0.0.1:9200/index/articles/1234"
-    };
-    request(opts, function (e, r) {
-
-      t.ok(r.found, "object created");
-      t.equals(r._source.name, "bob", "object updated");
-      t.equals(r._source.message, "yes", "original object properties not destroyed");
-      t.end();
-    });
-
-  });
-
+		t.ok(e, "error returned");
+		t.notOk(r, "no data passed to cb");
+		t.end();
+	});
 });
 
 test("#update should return err if no match in db", function (t) {
 
-  articles.update({
-    id: 4321
-  }, {
-    name: "bob"
-  }, function (e) {
+	var obj = {id: 4321};
 
-    t.ok(e, "error returned");
-    t.end();
-  });
+	var changes = {name: "bob"};
 
+	articles.update(obj, changes, function (e, r) {
+
+		t.notOk(e, "error returned");
+		t.ok(is.type(r, "object"), "object returned");
+		t.notOk(is.ok(r), "empty object returned");
+		t.end();
+	});
+});
+
+test("#update should update entry in database", function (t) {
+
+	var obj = {id: 1234};
+
+	var changes = {name: "bob"};
+
+	articles.update(obj, changes, function (e, r) {
+
+		t.notOk(e, "error not returned");
+		t.equals(r.message, "yes", "original properties still present");
+		t.equals(r.name, changes.name, "name updated");
+		t.end();
+	});
 });
 
 test("#update should return err if no id", function (t) {
 
-  articles.update({
-    hid: 4321
-  }, {
-    name: "bob"
-  }, function (e) {
+	var obj = {hid: 4321};
 
-    t.ok(e, "error returned");
-    t.end();
-  });
+	var changes = {name:"bob"};
+
+	articles.update(obj, changes, function (e, r) {
+
+		t.notOk(r, "r is null");
+		t.ok(e, "error returned");
+		t.end();
+	});
+});
+
+test("#del should not throw err if no match in db", function (t) {
+
+	var obj = {id:4321};
+
+
+	articles.del(obj, function (e, r) {
+
+		t.notOk(e, "error not returned");
+		t.ok(is.type(r, "object"), "object returned");
+		t.notOk(is.ok(r), "empty object returned");
+		t.end();
+	});
 
 });
 
 test("#del should delete entry from database", function (t) {
 
-  articles.del({
-    id: 1234
-  }, function (e, r) {
+	var obj = {id:"1234"};
 
-    t.ok(r.found, "record found");
-    t.notOk(e, "error not returned");
+	articles.del(obj, function (e, r) {
 
-    var opts = {
-      method: "GET",
-      uri: "http://127.0.0.1:9200/index/articles/1234"
-    };
-    request(opts, function (e, r) {
-      t.notOk(r.found, "object deleted");
-      t.end();
-    });
-  });
-});
-
-test("#del should throw err if no match in db", function (t) {
-
-  articles.del({
-    id: 1234
-  }, function (e) {
-
-    t.ok(e, "error returned");
-    t.end();
-  });
-
+		t.notOk(e, "no error");
+		t.ok(r, "record found");
+		t.equals(r.id, obj.id, "record returned");
+		t.end();
+	});
 });
 
 test("#del should throw err if no id", function (t) {
 
-  articles.del({
-    hid: 1234
-  }, function (e) {
+	var obj = {hid: "23780459"};
 
-    t.ok(e, "error returned");
-    t.end();
-  });
+	articles.del(obj, function (e, r){
 
+		t.ok(e, "error returned");
+		t.notOk(r, "no record returned");
+		t.end();
+	});
+});
+
+test("NOT A TEST wipe db", function (t) {
+
+	var opts = {
+		method: "DELETE",
+		uri: "http://127.0.0.1:9200/index/articles/"
+	};
+	request(opts, function () {
+
+		t.end();
+	});
 });
 
 
-test("wipe db", function (t) {
+test("Error database connection", function (t){
 
-  var opts = {
-    method: "DELETE",
-    uri: "http://127.0.0.1:9200/index/articles/"
-  };
-  request(opts, function () {
+	var opts = {
+		index: "index",
+		type: "articles",
+		// no transport protocol
+		host: "127.0.0.1",
+		port: Number(process.env.ES_PORT)
+	};
 
-    t.end();
-  });
+	var failArticles = instantiate(opts);
 
+	t.test("should trigger error in CREATE record", function (st){
+
+		var obj = {
+			message: "yes",
+			id: "1234"
+		};
+
+		failArticles.create(obj, function (e) {
+
+			st.ok(e, "error received");
+			st.end();
+		});
+	});
+
+	t.test("should trigger error in UPDATE record", function (st){
+
+		var obj = {
+			message: "yes",
+			id: "1234"
+		};
+
+		var changes = {};
+
+		failArticles.update(obj, changes, function (e) {
+
+			st.ok(e, "error received");
+			st.end();
+		});
+	});
+
+	t.test("should trigger error in UPDATE record", function (st){
+
+		var obj = {
+			message: "yes",
+			id: "1234"
+		};
+
+		try{
+			failArticles.update(obj, function () {
+
+				st.fail("should not have been called");
+			});
+		}catch(e){
+			st.ok(e, "should throw err");
+			st.end();
+		}
+	});
+
+	t.test("should trigger error in FINDONE record", function (st){
+
+		var obj = {
+			message: "yes",
+			id: "1234"
+		};
+
+		failArticles.findOne(obj, function (e) {
+
+			st.ok(e, "error received");
+			st.end();
+		});
+	});
+
+	t.test("should trigger error in DEL record", function (st){
+
+		var obj = {
+			id: "1234"
+		};
+
+		failArticles.del(obj, function (e) {
+
+			st.ok(e, "error received");
+			st.end();
+		});
+	});
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
